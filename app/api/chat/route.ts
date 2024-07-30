4eb6bf1c311a5c666db06fccb893b979341f8b8e
eufanzky <eufanzky@gmail.com>
2024-07-30 17:58:32 -0400

    first commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,15 @@
+import { google } from '@ai-sdk/google';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+    model: google("models/gemini-1.5-pro-latest"),
+    messages,
+  });
+  return result.toAIStreamResponse();
+}
