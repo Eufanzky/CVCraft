9ef59f8a5b815b1aef559b70f3594ffdab6c5b2b
eufanzky <eufanzky@gmail.com>
2024-07-31 09:58:52 -0400

    updated project structure

---
+++ src/app/about/pagex.tsx
@@ -0,0 +1,12 @@
+// pages/about.tsx
+import React from 'react';
+
+function About() {
+    return(
+        <div>
+            <p>About Page :D</p>
+        </div>
+    )
+}
+
+export default About;
